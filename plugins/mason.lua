7b1b521f32e32cd0ca15266975d1e337fdb1557a
qingu <jiangqingu@gmail.com>
2024-02-17 11:22:02 +0800

    add fortls etc. in Mason config

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -7,6 +7,7 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
+        "fortls", "pyright", "clangd", "bashls", "cmake",
         -- "lua_ls",
       })
     end,
